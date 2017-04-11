9d59325193e9bb20af3ed2144331d0f9aa7c5e0b
Pavel Matyáš <matyapav@fel.cvut.cz>
2017-04-12 00:20:58 +0200

    trying to run on gh pages

--- src/js/managers/resource_manager.js
+++ src/js/managers/resource_manager.js
@@ -6,10 +6,10 @@ export default class ResourceManager{
 
 
     static _resources = {
-        "trainer": '../img/trainer.png',
-        "grass" : '../img/grass.png',
-        "pokeball": '../img/pokeball.png',
-        "wall" : '../img/wall.png'
+        "trainer": './img/trainer.png',
+        "grass" : './img/grass.png',
+        "pokeball": './img/pokeball.png',
+        "wall" : './img/wall.png'
     };
 
     static get resources() {
